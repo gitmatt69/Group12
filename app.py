07c89888efb2329fe2d61f8bc977afcb2af8327f
gitmatt69 <u23739772@tuks.co.za>
2025-10-10 14:09:39 +0200

    Added a small piece of functionalty to the app.py file

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/suppliers')
+def suppliers():
+    return render_template('suppliers.html')
+
+@app.route('/orders')
+def orders():
+    return render_template('orders.html')
+
+@app.route('/reports')
+def reports():
+    return render_template('reports.html')
+
+@app.route('/performance')
+def performance():
+    return render_template('performance.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
