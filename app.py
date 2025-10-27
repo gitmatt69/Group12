93e5d9b8d6244c8f38dbd4b1392413b154c502b3
gitmatt69 <u23739772@tuks.co.za>
2025-10-27 10:52:52 +0200

    added a register page and made the navbar include register and login links

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ def get_db_connection():
 def login():
     return render_template('login.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 @app.route('/')
 def index():
     return render_template('index.html')
